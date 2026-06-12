33fb3e73fb96b8c033f2cdce9c64bac5aace50cf
Chetan Siddappareddy <chetansiddappareddy@gmail.com>
2026-06-11 19:37:15 -0500

    corrected models

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, JSON
 from sqlalchemy.sql import func
 from database import Base
 
